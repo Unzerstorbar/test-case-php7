7a19913b520caec37e121d5c9d4dbd68897110d5
Alexey Shamanin <alexowen5@icloud.com>
2020-11-19 14:15:48 +0300

    Add abstract Repository for database

---
+++ src/General/Infrastructure/Repository/Repository.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Components\General\Repository;
+
+use PDO;
+use PDOException;
+use PDOStatement;
+
+abstract class Repository
+{
+    protected PDO $db;
+
+    public static function create(): self
+    {
+        return new static();
+    }
+
+    /**
+     * Repository constructor
+     */
+    private function __construct()
+    {
+        try {
+            $this->db = new PDO(
+                "mysql:host={$_ENV['MYSQL_HOST']};port={$_ENV['MYSQL_PORT']};dbname={$_ENV['MYSQL_DATABASE']}",
+                $_ENV['MYSQL_USER'],
+                $_ENV['MYSQL_PASSWORD'],
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+                ]
+            );
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage());
+        }
+    }
+
+    public function query($stmt): PDOStatement
+    {
+        return $this->db->query($stmt);
+    }
+
+    public function prepare($stmt): PDOStatement
+    {
+        return $this->db->prepare($stmt);
+    }
+
+    public function lastInsertId(): string
+    {
+        return $this->db->lastInsertId();
+    }
+
+    public function run(string $query, array $args = []): PDOStatement
+    {
+        try {
+            if (!$args) {
+                return $this->query($query);
+            }
+            $stmt = $this->prepare($query);
+            $stmt->execute($args);
+            return $stmt;
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage());
+        }
+    }
+
+    public function getRows(string $query, array $args = []): array
+    {
+        return $this->run($query, $args)->fetchAll();
+    }
+}
